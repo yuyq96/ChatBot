4153d8ba0c9a5373c75fe064fbc3257e7f43d0f0
Archie Yu <yuyq96@126.com>
2018-02-13 23:43:03 +0800

    Add log for loading settings

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -3,6 +3,8 @@ from configparser import ConfigParser
 
 import os
 
+import logging
+
 
 def limit(num):
     return slimit(num)
@@ -20,8 +22,8 @@ PATH_MODULE = os.path.dirname(__file__) + "/../"
 PATH_DATA = PATH_MODULE + "data/"
 
 CONFIG_FILE = PATH_MODULE + "ChatBot.cfg"
-print(os.path.abspath(CONFIG_FILE))
 if os.path.exists(CONFIG_FILE):
+    logging.info("load %s" % os.path.abspath(CONFIG_FILE))
     config = ConfigParser()
     config.read(CONFIG_FILE)
     PATH_FQA = PATH_DATA + config.get("CUSTOM", "PATH_FQA")
@@ -32,3 +34,5 @@ if os.path.exists(CONFIG_FILE):
     WECHAT_APP_SECRET = config.get("CUSTOM", "WECHAT_APP_SECRET")
     TXT_NO_ANSWER = config.get("CUSTOM", "TXT_NO_ANSWER")
     TXT_MEANINGLESS_ANSWER = config.get("CUSTOM", "TXT_MEANINGLESS_ANSWER")
+else:
+    logging.error("%s exist" % os.path.abspath(CONFIG_FILE))
