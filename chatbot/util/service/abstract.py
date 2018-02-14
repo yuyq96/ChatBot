b6972e0692d40a9ba27b29788f6917114f7c5142
Archie Yu <yuyq96@126.com>
2018-02-14 22:27:36 +0800

    Fix server start log

--- chatbot/util/service/abstract.py
+++ chatbot/util/service/abstract.py
@@ -22,9 +22,9 @@ class Service:
 
     def start(self):
         if self.bot:
-            self._start()
             self.running = True
-            logging.error("Service started")
+            logging.info("Service started")
+            self._start()
 
     def stop(self):
         if not self.running:
